0d0689ab800edd26c493dee1df7d38be5f5cb798
LAPTOP-47H6LM19\USER <ebitabetai7@gmail.com>
2018-05-06 10:26:31 +0900

    引数boolean課題コミット

---
+++ 011.メソッド/test3.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author USER
+ */
+@WebServlet(urlPatterns = {"/test3"})
+public class test3 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    void hikisu2(int num,int num2,boolean pass,PrintWriter pw){
+        int nums = num * num2;
+        if(pass == true){
+           nums = nums * nums;
+           pw.print(nums + "号番は正常です！<br>");
+           pw.print("<br>");
+        } else{
+           pw.print(nums + "号番は違います。やり直してください！<br>");
+           pw.print("<br>");
+        }
+        
+    }
+    
+    void hikisu2 (PrintWriter pw){
+        hikisu2(5, 1,false,pw);
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           hikisu2(3,2,true,out);
+           hikisu2(1,4,false,out);
+           hikisu2(8,6,true,out);
+           hikisu2(out);
+           hikisu2(7,3,false,out);
+           hikisu2(6,4,true,out);
+           
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
