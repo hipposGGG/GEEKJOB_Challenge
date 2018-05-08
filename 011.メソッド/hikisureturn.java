456a6861c61d11152955fdae76a9c7fbc1353534
LAPTOP-47H6LM19\USER <ebitabetai7@gmail.com>
2018-05-08 18:04:10 +0900

    引数、戻り値１　課題提出　コミット

---
+++ 011.メソッド/hikisureturn.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author USER
+ */
+@WebServlet(urlPatterns = {"/hikisureturn"})
+public class hikisureturn extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    String[] UsProfile(String id){
+        String[][] profiles = {
+            {"1", "1993年07月27日", "神奈川県横浜市"},
+            {"2", "2003年08月27日", "山形県山形市"},
+            {"3", "2013年05月27日", "福岡県福岡市"}
+        };
+        for(String[] profile : profiles){
+            if(profile[0].equals(id)){
+                return profile;
+            }      
+        }
+        return null;
+    }
+       
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String id = "1";
+            String[] a = UsProfile(id);
+            for (int i = 1 ; i < a.length; i++) {
+                out.print(a[i]);
+            }
+    }
+    }
+//            for (int i = 0; i < Myprofile().length; i++) {
+//                out.print( Myprofile()[i] + "<br>");  
+//            }                       
+    
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
